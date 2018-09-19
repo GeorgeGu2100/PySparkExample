1dd2235224d0d8bd4b5c2ee5ced16c22e2a29240
ggu2100 <GeorgeGu2100@users.noreply.github.com>
2018-09-19 07:45:04 -0500

    include_codes_calculate_HF_score

--- Med_Data_Analysis_DataFrame.py
+++ Med_Data_Analysis_DataFrame.py
@@ -29,6 +29,27 @@ COPD_column_list = ('HistoryofMechanicalVentilation', 'SleepApnea', 'Respiratord
 'HypertensiveHeartandRenalDiseaseorEncephalopathy')
 
 
+HF_diagnosis_code_list=['402.01', '402.11', '402.91', '404.01', '404.03', '404.11', '404.13', '404.91', '404.93', '428.xx']
+HF_column_list = ('HistoryofCABG', 'Septicemia/shock', 'Congestiveheartfailure', 'Acutecoronarysyndrome', 'Coronaryatherosclerosisoranginacerebrovasculardisease', 
+'Valvularorrheumaticheartdisease', 'Specifiedarrhythmias', 'Vascularorcirculatorydisease', 'OtherandUnspecifiedHeartDisease', 'Metastaticcanceroracuteleukemia', 'Cancer', 
+'Diabetesmellitus(DM)orDMcomplications', 'Protein-caloriemalnutrition', 'Disordersoffluidelectrolyteacid-base', 'Liverandbiliarydisease', 
+'PepticUlcerHemorrhageOtherSpecifiedGastrointestinalDisorders', 'OtherGastrointestinalDisorders', 'SevereHematologicalDisorders', 'Irondeficiencyorotheranemiasandblooddisease',
+'Dementiaorotherspecifiedbraindisorders', 'Drug/AlcoholInducedDependence/Psychosis', 'MajorPsychiatricDisorders', 'Depression', 'OtherPsychiatricDisorders', 
+'Hemiplegiaparaplegiaparalysisfunctionaldisability', 'Stroke', 'Chronicobstructivepulmonarydisease', 'Fibrosisoflungandotherchroniclungdisorders', 'Asthma', 'Pneumonia', 
+'End-stagerenaldiseaseordialysis', 'Renalfailure', 'Nephritis', 'Otherurinarytractdisorders', 'Decubitusulcerorchronicskinulcer')
+
+
+HWR_diagnosis_code_list=['CCS']
+HWR_column_list = ('Historyofinfection', 'Pneumonia', 'Metastaticcanceroracuteleukemia', 'LungUpperDigestiveTractandOtherSevereCancers', 
+'LymphaticHeadandNeckBrainandOtherMajorCancers;BreastColorectalandotherCancersandTumors;OtherRespiratoryandHeartNeoplasms', 'Diabetesmellitus(DM)orDMcomplications', 
+'Protein-caloriemalnutrition', 'End-stageliverdisease', 'Coagulationdefectsandotherspecifiedhematologicaldisorders', 'Drug/AlcoholInducedDependence/Psychosis', 
+'MajorPsychiatricDisorders','Hemiplegiaparaplegiaparalysisfunctionaldisability', 'Seizuredisordersandconvulsions', 'Chronicheartfailure', 
+'Coronaryatherosclerosisoranginacerebrovasculardisease', 'Specifiedarrhythmias', 'Chronicobstructivepulmonarydisease', 'Fibrosisoflungandotherchroniclungdisorders', 
+'Dialysisstatus', 'Decubitusulcerorchronicskinulcer', 'Septicemia/shock', 'Disordersoffluidelectrolyteacid-base', 'Irondeficiencyorotheranemiasandblooddisease', 'Renalfailure', 
+'PancreaticDisease', 'Rheumatoidarthritisandinflammatoryconnectivetissuedisease', 'Respiratordependence/tracheostomystatus', 'Transplants', 
+'Coagulationdefectsandotherspecifiedhematologicaldisorders', 'Hipfracture/dislocation')
+
+
 # get the commodity_score for a certain measure
 # based on counts of 'Yes' over a list of columns
 # input: args = () -- list_of_columns for a certain measure
@@ -107,8 +128,7 @@ def main(arg):
     print('data loaded')
     # print(Dataset.printSchema())
     # print(Dataset.show(2,truncate = True))
-    # print(Dataset.filter(Dataset.diagnosis_code.isin(AMI_diagnosis_code_list)).select("encounter_id","age","diagnosis_code","Protein-caloriemalnutrition","OtherEndocrine/Metabolic/NutritionalDisorders").show(2,truncate = True))
-    
+
     if(arg == 'AMI'):
         Dataset = Dataset.withColumn('ami_commodity_score',commodity_score_udf(*AMI_column_list)) \
         .withColumn('ami_lace_score',lace_score_udf('LengthOfStay','Inpatient_visits','ami_commodity_score','ED_visits'))
@@ -121,11 +141,12 @@ def main(arg):
         print("total_counts: ",total_counts)
         print("effective_counts: ",effective_counts)
         print("AMI_score: ",AMI_score)
+        
     elif(arg == 'COPD'):
         Dataset = Dataset.withColumn('copd_commodity_score',commodity_score_udf(*COPD_column_list)) \
         .withColumn('copd_lace_score',lace_score_udf('LengthOfStay','Inpatient_visits','copd_commodity_score','ED_visits'))
         print(Dataset.filter(Dataset.diagnosis_code.isin(COPD_diagnosis_code_list)).select('encounter_id','race','gender','age','diagnosis_code','LengthOfStay','Inpatient_visits','copd_commodity_score','ED_visits','copd_lace_score').show(5,truncate = True))
-    
+
         # calculate counts and final measure score
         total_counts = Dataset.count()
         effective_counts = Dataset.filter("copd_lace_score>9").count()
@@ -133,6 +154,19 @@ def main(arg):
         print("total_counts: ",total_counts)
         print("effective_counts: ",effective_counts)
         print("COPD_score: ",COPD_score)        
+
+    elif(arg == 'HF'):
+        Dataset = Dataset.withColumn('hf_commodity_score',commodity_score_udf(*HF_column_list)) \
+        .withColumn('hf_lace_score',lace_score_udf('LengthOfStay','Inpatient_visits','hf_commodity_score','ED_visits'))
+        print(Dataset.filter(Dataset.diagnosis_code.isin(HF_diagnosis_code_list)).select('encounter_id','race','gender','age','diagnosis_code','LengthOfStay','Inpatient_visits','hf_commodity_score','ED_visits','hf_lace_score').show(5,truncate = True))
+
+        # calculate counts and final measure score
+        total_counts = Dataset.count()
+        effective_counts = Dataset.filter("hf_lace_score>9").count()
+        HF_score = float(effective_counts)/float(total_counts)
+        print("total_counts: ",total_counts)
+        print("effective_counts: ",effective_counts)
+        print("HF_score: ",HF_score)               
         
     elif(arg == None):
         print('Not an effective measure name.')
